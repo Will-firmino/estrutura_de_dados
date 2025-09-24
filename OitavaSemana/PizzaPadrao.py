147e5a6aba1ef222d304e21bc85798f79e419338
William Firmino <prof.williamfirmino@gmail.com>
2025-09-24 10:22:46 -0300

    Subindo alterações antes do intervalo do dia 24/09

---
+++ OitavaSemana/PizzaPadrao.py
@@ -0,0 +1,22 @@
+class PizzaPadrao:
+    def __init__(self, sabor, tamanho, preco, ingredientes):
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco = preco
+        self.ingredientes = ingredientes
+
+    def mostrar_detalhes(self):
+        print(f'''
+        ---🍕 Detalhes da Pizza ---
+                Sabor: {self.sabor}
+                Tamanho: {self.tamanho}
+                Preço: {self.preco}
+                Ingredientes: {self.ingredientes}
+            ''')
+        
+pizza_calabresa = PizzaPadrao("Calabresa", "Pequeno", 50.00, "Tomate, oregano")
+pizza_marguerita = PizzaPadrao("Marguerita", "Médio", 55.00, "Tomate, oregano, queijo")
+
+pizza_calabresa.mostrar_detalhes()
+pizza_marguerita.mostrar_detalhes()
+
